b8924654b74a105ae10e6ada6db473f4136bdb25
Mukha Heidari <saidhdr@gmail.com>
2017-04-13 10:54:16 +0200

    Added get api

--- __init__.py
+++ __init__.py
@@ -26,6 +26,50 @@ def getIsUserKnown():
     userid = request.args.get('username', None)
     return jsonify(Controller.isUserKnown(userid))
 
+@app.route("/risk/isclientknown", methods=['GET'])
+def getIsClientKnown():
+    mac_address = request.args.get('mac', None)
+    return jsonify(Controller.isClientKnown(mac_address))
+    
+
+@app.route("/risk/isipknown", methods=['GET'])
+def getIsIpKnown():
+    ip = request.args.get('ip', None)
+    return jsonify(Controller.isIpTypeOf(ip,IpType.known))
+    
+
+
+@app.route("/risk/isipinternal", methods=['GET'])
+def getIsIpInternal():
+    ip = request.args.get('ip', None)
+    return jsonify(Controller.isIpTypeOf(ip,IpType.internal))
+    
+
+
+@app.route("/risk/lastsuccessfullogindate", methods=['GET'])
+def getLastSuccessfulLoginDate():
+    userid = request.args.get('username', None)
+    result = Controller.getLastSuccessfulLoginDate(userid)
+    if len(result) > 0:
+        return jsonify(result[0].split()[0])
+    else:
+        return 'NA'
+
+
+@app.route("/risk/LastFailedLoginDate", methods=['GET'])
+def getLastFailedLoginDate():
+    userid = request.args.get('username', None)
+    result = Controller.getLastFailedLoginDate(userid)
+    if len(result) > 0:
+        return jsonify(result[0].split()[0])
+    else:
+        return 'NA'
+
+
+@app.route("/risk/failedlogincountlastweek", methods=['GET'])
+def getFailedLoginCountLastWeek():
+    return jsonify(len(Controller.getFailedLoginCountLastWeek()))
+
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=8080, threaded=True, debug=True)
