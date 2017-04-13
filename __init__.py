556858b7181ee943e3374f2592eaaffcca4e19ae
Mukha Heidari <saidhdr@gmail.com>
2017-04-13 09:55:33 +0200

    Added dummy log file and testing using class in pytho, it's coooool

--- __init__.py
+++ __init__.py
@@ -1,10 +1,18 @@
 from flask import Flask, jsonify
+from localController import MyMethods
+
+Controller = MyMethods()
 app = Flask(__name__)
 
 
 @app.route("/")
+@app.route("/index")
 def index():
     return jsonify("hello, world")
+
+@app.route("/hello")
+def hello():
+    return jsonify(Controller.hellowWorld())
 
 
 if __name__ == "__main__":
---
+++ localController.py
@@ -0,0 +1,10 @@
+from distutils.util import strtobool
+
+
+class MyMethods:
+    def __init__(self):
+       self.temp = []
+
+ 
+    def hellowWorld(self):
+        return "hello, world from class"
