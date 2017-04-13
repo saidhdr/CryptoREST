aaec3f893312db69fb4d82631b1666235997138d
Mukha Heidari <saidhdr@gmail.com>
2017-04-13 10:11:36 +0200

    fixed an mismatched in route name

--- __init__.py
+++ __init__.py
@@ -15,5 +15,9 @@ def hello():
     return jsonify(Controller.hellowWorld())
 
 
+@app.route("/getalllog")
+def getAllLog():
+    return jsonify(Controller.getLatestLog())
+
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=8080, threaded=True, debug=True)
--- localController.py
+++ localController.py
@@ -3,8 +3,16 @@ from distutils.util import strtobool
 
 class MyMethods:
     def __init__(self):
-       self.temp = []
+        self.logFileLines = []
+        self.logFileLines = [line.rstrip('\n') for line in open('static/dummyLog.txt', 'r')]
 
  
     def hellowWorld(self):
         return "hello, world from class"
+
+    def getLatestLog(self):
+        result = []    
+        self.logFileLines = [line.rstrip('\n') for line in open('static/dummyLog.txt', 'r')]
+        result.extend(self.logFileLines)
+        
+        return result
